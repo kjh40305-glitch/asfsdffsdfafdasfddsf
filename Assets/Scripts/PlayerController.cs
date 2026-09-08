a8bd2f946f21fa0b9fc87425e4901f8b946b0aab
JHK2021 <kim16729@naver.com>
2026-09-08 15:32:45 +0900

    09081532
    asddaa

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,10 +1,20 @@
 using UnityEngine;
+using UnityEngine.AI;
 using UnityEngine.InputSystem;
+using UnityEngine.UIElements;
 
 public class PlayerController : MonoBehaviour
 {
+    private float elapsedTime = 0f;
+
+    float score = 0f;
+    public float scoreMultiplier = 10f;
     public float thrustForce = 1f;
     Rigidbody2D rb;
+    
+    public UIDocument uiDocument;
+
+    Label scoreText;
 
     public float maxSpeed = 5f;
     public GameObject boost;
@@ -12,6 +22,7 @@ public class PlayerController : MonoBehaviour
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
+        scoreText = uiDocument.rootVisualElement.Q<Label>("ScoreLabel");
     }
 
     // Update is called once per frame
@@ -19,17 +30,24 @@ public class PlayerController : MonoBehaviour
     {
         if (Mouse.current.leftButton.isPressed)
         {
-            Vector3 mousePos = Camera.main.ScreenToWorldPoint(Mouse.current.position.value);
-            Debug.Log(mousePos);
+            elapsedTime += Time.deltaTime;
+            score = Mathf.FloorToInt(elapsedTime * scoreMultiplier);
 
-            Vector2 direction = (mousePos - transform.position).normalized;
-            transform.up = direction;
-
-            rb.AddForce(direction * thrustForce);
-
-            if (rb.linearVelocity.magnitude > maxSpeed)
+            scoreText.text = "Score : " + score;
+            if (Mouse.current.leftButton.isPressed)
             {
-                rb.linearVelocity = rb.linearVelocity.normalized * maxSpeed;
+                Vector3 mousePos = Camera.main.ScreenToWorldPoint(Mouse.current.position.value);
+                Debug.Log(mousePos);
+
+                Vector2 direction = (mousePos - transform.position).normalized;
+                transform.up = direction;
+
+                rb.AddForce(direction * thrustForce);
+
+                if (rb.linearVelocity.magnitude > maxSpeed)
+                {
+                    rb.linearVelocity = rb.linearVelocity.normalized * maxSpeed;
+                }
             }
         }
 
